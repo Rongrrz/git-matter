872707fef5145149ae9d06c91f1502fee0fb9ef2
Rongrui Zhu <ronrzhu@gmail.com>
2026-05-13 18:18:50 -0700

    feat: add dirtyTracker utility

---
+++ src/utils/dirtyTracker.ts
@@ -0,0 +1,9 @@
+export function createDirtyTracker<T>(getValue: () => T) {
+    let previous = getValue();
+    return (): boolean => {
+        const current = getValue();
+        const isDirty = current !== previous;
+        previous = current;
+        return isDirty;
+    }
+}
