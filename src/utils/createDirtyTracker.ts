2ee272e94c426e277203a8e1432d9c1b1ba3b076
Rongrui Zhu <ronrzhu@gmail.com>
2026-05-14 18:10:58 -0700

    refactor: remove legacy util functions

--- src/utils/createDirtyTracker.ts
+++
@@ -1,9 +0,0 @@
-export function createDirtyTracker<T>(getValue: () => T) {
-  let previous = getValue();
-  return (): boolean => {
-    const current = getValue();
-    const isDirty = current !== previous;
-    previous = current;
-    return isDirty;
-  };
-}
